7f4be8814caa643ea335463b29f5bb4bc493ddef
scazan <scottcazan@gmail.com>
2022-02-09 15:14:31 +0100

    add Pchoose

---
+++ src/Pchoose.ts
@@ -0,0 +1,24 @@
+import { unwrapValue } from './pattern-utils';
+import { getSequentialRandomIndex } from './utils';
+
+export default function* Pchoose(values: Array<any>, repetitions?: number){
+  let lastIndex = 0;
+
+  const result = (): any => {
+    const nextIndex = getSequentialRandomIndex(lastIndex, values.length);
+    const nextElement = values[nextIndex];
+
+    return unwrapValue(nextElement);
+  };
+
+  if(repetitions == undefined) {
+    while(true) {
+      yield result();
+    }
+  }
+  else {
+    for(let i=0; i<repetitions; i++) {
+      yield result();
+    }
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export const Pattern = (pattern: any) => [() => pattern.next().value];
 
 export { default as Pseq } from './Pseq';
 export { default as Prand } from './Prand';
+export { default as Pchoose } from './Pchoose';
 export { default as Pmarkov } from './Pmarkov';
 export { default as Pgenetic } from './Pgenetic';
 export { default as utils } from './utils';
